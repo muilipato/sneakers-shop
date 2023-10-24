36d9f1f8a3159d5d753d41179ad059392b9b9795
Patrick <muilipato@gmail.com>
2023-10-24 18:21:56 +0300

    feat:toggle added on menu. Js function and a show class in the sacss file

---
+++ src/js/index.js
@@ -0,0 +1,15 @@
+window.onload = ()=>{
+const showMenu = (toggleId,navId) =>{
+    const toggle = document.getElementById(toggleId),
+     nav = document.getElementById(navId)
+
+    if(toggle && nav){
+    toggle.addEventListener('click', ()=>{
+        nav.classList.toggle('show')
+
+     })
+
+    }
+}
+showMenu('nav-toggle','nav-menu')
+}
